38abc6cd98d73ac623dc3dc41f6592f07d1a338a
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-05-13 13:29:29 +0200

    Updata the 1st day of the 3rd effort for the main challenge.

--- main_challenge/main_challenge_effort_03_day_01/main.cpp
+++ main_challenge/main_challenge_effort_03_day_01/main.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
 #include <thread>
+#include <memory>
+
+class IntData {
+    public:
+        IntData()
+            : _i(0) {
+            std::cout << "Hello Int." << '\n';
+        }
+
+        IntData(int i)
+            : _i(i) {
+            std::cout << "Hello Int." << '\n';
+        }
+
+        ~IntData() {
+            std::cout << "Bye Int." << '\n';
+        }
+
+        int getData() {
+            return _i;
+        }
+
+        void setData(int i) {
+            _i = i;
+        }
+
+    private:
+        int _i;
+};
+
+class MyData {
+    public:
+        MyData()
+            : _i(new IntData()) {
+        }
+
+        std::shared_ptr<IntData> getData() {
+            return _i;
+        }
+
+        void setData(IntData i) {
+            *_i = i;
+        }
+
+    private:
+        std::shared_ptr<IntData> _i;
+};
 
 class Hello {
     public: 
@@ -13,6 +60,10 @@ class Hello {
 
         void operator()() {
             std::cout << "Some activity." << '\n';
+            MyData myData;
+            std::cout << myData.getData()->getData() << '\n';
+            myData.setData(IntData(10));
+            std::cout << myData.getData()->getData() << '\n';
         }
 };
 
