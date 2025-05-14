ac9df9ee3caeb9243a776b1c6d6d1a154e92d8cc
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-05-14 16:11:09 +0200

    Add the 3rd day of the 3rd effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_03_day_02/main.cpp
@@ -0,0 +1,9 @@
+#include "stack.hpp"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello." << '\n';
+    Stack stack;
+    return 0;
+}
---
+++ main_challenge/main_challenge_effort_03_day_02/stack.cpp
@@ -0,0 +1,7 @@
+#include "stack.hpp"
+
+#include <iostream>
+
+Stack::Stack() {
+    std::cout << "Stack constructor." << '\n';
+}
