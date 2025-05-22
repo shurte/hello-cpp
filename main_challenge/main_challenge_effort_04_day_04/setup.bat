fd615a2e19745e0fcb79a60a877349bd6f958904
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-05-22 08:26:48 +0200

    Add the 4th day of the 4th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_04_day_04/setup.bat
@@ -0,0 +1,4 @@
+set PATH=%PATH%;c:/_programs/msys64/ucrt64/bin
+:: here "c:/_programs/msys64/ucrt64/bin" is the folder that contains "g++.exe" to compile c++ files
+set PATH=%PATH%;c:/_programs/cmake-4.0.1-windows-x86_64/bin
+:: here "c:/_programs/cmake-4.0.1-windows-x86_64/bin" is the folder that contains "cmake" to use CMake in the workspace
