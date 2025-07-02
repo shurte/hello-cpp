4c01c5f4c9a666978e52fbc602f423579f0694a3
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-07-02 16:07:57 +0200

    Add the 15th day of the 7th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_07_day_15/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+class MainGame {
+
+public:
+    int init() {
+        SDL_SetMainReady();
+        int initResult = SDL_Init(SDL_INIT_VIDEO);
+
+        if (initResult < 0) {
+            std::cout << "could not initialize sdl2: " << SDL_GetError() << '\n';
+            return 1;
+        }
+
+        window = SDL_CreateWindow(
+                "hello_sdl2",
+                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                SCREEN_WIDTH, SCREEN_HEIGHT,
+                SDL_WINDOW_SHOWN
+            );
+
+        if (window == NULL) {
+            std::cout << "could not create window: " << SDL_GetError() << '\n';
+            return 1;
+        }
+
+        SDL_Surface* screenSurface = NULL;
+        screenSurface = SDL_GetWindowSurface(window);
+        
+        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+        SDL_UpdateWindowSurface(window);
+
+        return 0;
+    }
+
+    void run_loop() {
+        SDL_Event event;
+        bool isRunning = true;
+
+        while (isRunning) {
+            while (SDL_PollEvent(&event)) {
+                switch (event.type) {
+                    case SDL_QUIT:
+                        isRunning = false;
+                        break;
+                }
+            }
+        }
+    }
+
+    void close() {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+private:
+    SDL_Window* window = NULL;
+};
+
+
+int main() {
+    MainGame window;
+    if (window.init()) {
+        window.run_loop();
+        window.close();
+    }
+    return 0;
+}
---
+++ main_challenge/main_challenge_effort_07_day_15/setup.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;c:/_programs/msys64/ucrt64/bin
+set SDL_HOME=c:/_development/sandbox/cpp/libs/SDL2-2.32.8/x86_64-w64-mingw32/include/SDL2
