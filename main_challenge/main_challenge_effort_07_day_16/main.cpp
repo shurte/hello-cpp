d41a143d62068dd0b481dba168954f07adaf5c24
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-07-03 14:59:29 +0200

    Add the 16th day of the 7th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_07_day_16/main.cpp
@@ -0,0 +1,43 @@
+#include <SDL2/SDL.h>
+
+class MainWindow {
+    public:
+        void init() {
+            SDL_Init(SDL_INIT_VIDEO);
+            _windowPtr = SDL_CreateWindow("Hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN);
+            SDL_Surface* surface = NULL;
+            surface = SDL_GetWindowSurface(_windowPtr);
+            SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(_windowPtr);
+        }
+
+        void runLoop() {
+            SDL_Event event;
+            bool isRunning = true;
+            while (isRunning) {
+                while(SDL_PollEvent(&event)) {
+                    switch (event.type) {
+                        case SDL_QUIT:
+                            isRunning = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        void close() {
+            SDL_DestroyWindow(_windowPtr);
+            SDL_Quit();
+        }
+
+    private:
+        SDL_Window* _windowPtr = nullptr;
+};
+
+int main(int argc, char** argv) {
+    MainWindow window;
+    window.init();
+    window.runLoop();
+    window.close();
+    return 0;
+}
