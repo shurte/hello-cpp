44053f1f620cc8fcc27be102506df49bb4053e4f
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-07-07 13:34:17 +0200

    Add the 1st day of the 8th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_08_day_01/setup.bat
@@ -0,0 +1,3 @@
+set PATH=%PATH%;c:/_programs/msys64/ucrt64/bin
+set PATH=%PATH%;c:/_programs/cmake-4.0.1-windows-x86_64/bin
+set SDL_HOME=c:\_development\sandbox\cpp\libs\SDL2-2.32.8\x86_64-w64-mingw32
