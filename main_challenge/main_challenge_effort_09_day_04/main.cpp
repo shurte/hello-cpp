c91c6ccbdec3e66a259dd463359bdfea38493e05
Alex Makarau <aliaksandr.makarau@gmail.com>
2025-07-17 13:59:20 +0200

    Add the 4th day of the 9th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_09_day_04/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+#include <GL/glew.h>
+
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 position;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+int main(int argc, char** argv) {
+    std::cout << "Hello" << '\n';
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        500,
+        500,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    std::cout << "Hello" << '\n';
+
+    glewExperimental = GL_TRUE;
+    GLint initResult = glewInit();
+    std::cout << GLEW_OK << '\n';
+    std::cout << initResult << '\n';
+
+    SDL_GL_MakeCurrent(window, context);
+
+    glViewport(0, 0, 500, 500);
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLfloat firstTriangle[] = {
+        -0.9f, -0.5f, 0.0f,  // Left 
+        -0.0f, -0.5f, 0.0f,  // Right
+        -0.45f, 0.5f, 0.0f,  // Top 
+    };
+    GLfloat secondTriangle[] = {
+        0.0f, -0.5f, 0.0f,  // Left
+        0.9f, -0.5f, 0.0f,  // Right
+        0.45f, 0.5f, 0.0f   // Top 
+    };
+    GLuint VBOs[2], VAOs[2];
+    glGenVertexArrays(2, VAOs); // We can also generate multiple VAOs or buffers at the same time
+    glGenBuffers(2, VBOs);
+
+    glBindVertexArray(VAOs[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);	// Vertex attributes stay the same
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    glBindVertexArray(VAOs[1]);	// Note that we bind to a different VAO now
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	// And a different VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0); // Because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out.
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    SDL_Event event;
+    bool isRunning = true;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
+        }
+
+        glClearColor(0.0f, 0.6f, 0.6f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(VAOs[1]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
---
+++ main_challenge/main_challenge_effort_09_day_04/setup.bat
@@ -0,0 +1,13 @@
+set PATH=%PATH%;c:/_programs/msys64/ucrt64/bin
+set PATH=%PATH%;c:/_programs/cmake-4.0.1-windows-x86_64/bin
+
+set SDL_HOME=c:/_development/sandbox/cpp/libs/SDL2-2.32.8/x86_64-w64-mingw32/
+set GLEW_HOME=c:/_development/sandbox/cpp/libs/glew-2.1.0/
+
+set SDL_INCLUDE=%SDL_HOME%include
+set SDL_LIB=%SDL_HOME%lib
+set SDL_BIN=%SDL_HOME%bin
+
+set GLEW_INCLUDE=%GLEW_HOME%include
+set GLEW_LIB=%GLEW_HOME%lib/Release/x64
+set GLEW_BIN=%GLEW_HOME%bin/Release/x64
