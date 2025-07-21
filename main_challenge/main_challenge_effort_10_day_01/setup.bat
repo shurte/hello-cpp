dbdaefb7d251771ed464a0bc68e81fa7d4e337c5
Ales Makarau <aliaksandr.makarau@gmail.com>
2025-07-21 10:26:38 +0200

    Add the 1st day of the 10th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_10_day_01/setup.bat
@@ -0,0 +1,6 @@
+set PATH=%PATH%;c:/_programs/mingw64/bin
+
+set SDL_HOME=c:/_development/sandbox/cpp/lib/SDL
+set SDL_INCLUDE=%SDL_HOME%/include
+set SDL_LIB=%SDL_HOME%/lib
+set SDL_BIN=%SDL_HOME%/bin
