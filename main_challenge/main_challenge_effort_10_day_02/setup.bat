7a3b52f58e59b93afffd15d54627de715d5fe0af
Ales Makarau <aliaksandr.makarau@gmail.com>
2025-07-22 19:50:07 +0200

    Add the 2nd day of the 10th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_10_day_02/setup.bat
@@ -0,0 +1,11 @@
+set PATH=%PATH%;c:/_programs/mingw64/bin
+
+set SDL_HOME=c:/_development/sandbox/cpp/lib/SDL/
+set SDL_INCLUDE=%SDL_HOME%include
+set SDL_LIB=%SDL_HOME%lib
+set SDL_BIN=%SDL_HOME%bin
+
+set GLEW_HOME=c:/_development/sandbox/cpp/lib/glew-2.1.0/
+set GLEW_INCLUDE=%GLEW_HOME%include
+set GLEW_LIB=%GLEW_HOME%lib/Release/x64
+set GLEW_BIN=%GLEW_HOME%bin/Release/x64
