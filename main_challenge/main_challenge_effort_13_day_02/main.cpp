18c808961132309a808ea8ca2ef2cbbc2ac022ba
Ales Makarau <aliaksandr.makarau@gmail.com>
2025-08-18 18:07:55 +0200

    Add the 2nd day of the 13th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_13_day_02/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+#include <GL/glew.h>
+
+int main() {
+    std::cout << "Hello" << '\n';
+
+    SDL_Init(SDL_INIT_EVERYTHING);    
+    
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    glewExperimental = GL_TRUE;
+    GLenum error = glewInit();
+    std::cout << "Init result: " << error << '\n';
+
+    SDL_Event event;
+    bool isRunning = true;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
+        }
+
+        glClearColor(0.0f, 0.6f, 0.6f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
