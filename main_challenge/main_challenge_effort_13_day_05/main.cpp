be155825d3682925f104487f481ea096a4f7554c
Ales Makarau <aliaksandr.makarau@gmail.com>
2025-08-21 19:58:17 +0200

    Add the 5th day of the 13 effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_13_day_05/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Hello {
+    public:
+        Hello() {
+            std::cout << "Hello" << '\n';
+        }
+
+        ~Hello() {
+            std::cout << "Bye" << '\n';
+        }
+};
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+int main() {
+    Hello hello;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_Event event;
+    bool isRunning = true;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
