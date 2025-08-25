3472934f1e3ab13b4c7eb087afe3c86d986dacb8
Ales Makarau <aliaksandr.makarau@gmail.com>
2025-08-25 12:55:13 +0200

    Add the 9th day of the 13th effort for the main challenge.

---
+++ main_challenge/main_challenge_effort_13_day_09/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+class Hello {
+    public:
+        Hello() {
+            std::cout << "Hello" << '\n';
+        }
+
+        ~Hello() {
+            std::cout << "Bye" << '\n';
+        }
+};
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+#include <GL/glew.h>
+
+#include <thread>
+
+void fooRectangle(GLuint VBO[1], GLuint VAO[1]) {
+    GLfloat rectangle[] = {
+        -0.9f, -0.5f, 0.0f,
+        0.9f, -0.5f, 0.0f,
+        0.9f, 0.5f, 0.0f,
+        -0.9f, 0.5f, 0.0f
+    };
+
+    glGenVertexArrays(1, VAO);
+    glGenBuffers(1, VBO);
+
+    glBindVertexArray(VAO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+}
+
+int main() {
+    Hello hello;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    std::cout << "glew init: " << glewInit() << '\n';
+
+    SDL_GL_MakeCurrent(window, context);
+    GLuint VBO[1], VAO[1];
+
+    fooRectangle(VBO, VAO);
+
+    SDL_Event event;
+    bool isRunning = true;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
+        }
+
+        glClearColor(0.0f, 0.6f, 0.6f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO[0]);
+        glDrawArrays(GL_POLYGON, 0, 4);
+        glBindVertexArray(0);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
