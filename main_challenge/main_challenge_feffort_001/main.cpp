cf12b86ad98d7952b06c791b8d8c580cd75e92e4
Ales Makarau <aliaksandr.makarau@gmail.com>
2025-09-03 20:52:03 +0200

    Update the 1st feffort for the main challenge.

--- main_challenge/main_challenge_feffort_001/main.cpp
+++ main_challenge/main_challenge_feffort_001/main.cpp
@@ -3,11 +3,40 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
+#include <GL/glew.h>
+
 int main() {
     Hello hello;
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    glewInit();
+
+    SDL_Event event;
+    bool isRunning = true;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
