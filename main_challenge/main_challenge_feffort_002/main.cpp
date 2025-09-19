320975eb224cdf122a6e7fe066289eb0aadeea4b
Alex <aliaksandr.makarau@gmail.com>
2025-09-19 14:47:55 +0200

    Update the 2nd feffort for the main challenge.

--- main_challenge/main_challenge_feffort_002/main.cpp
+++ main_challenge/main_challenge_feffort_002/main.cpp
@@ -1,33 +1,92 @@
-#include <iostream>
-#include <string>
-#include <thread>
+#include <hello.hpp>
 
-void print(const std::string& message) {
-    std::cout << message.c_str() << '\n';
-}
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
-void printHello() {
-    print("Hello");
-}
-
-void printBye() {
-    print("Bye");
-}
-
-class Hello {
-    public:
-        Hello() {
-            std::thread helloThread(printHello);
-            helloThread.join();
-        }
-
-        ~Hello() {
-            std::thread byeThread(printBye);
-            byeThread.join();
-        }
-};
+#include <GL/glew.h>
 
 int main() {
     Hello hello;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1200,
+        600,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    glewInit();
+    SDL_GL_MakeCurrent(window, context);
+
+    GLuint VBO[1];
+    GLuint VAO[1];
+
+    glGenVertexArrays(1, VAO);
+    glGenBuffers(1, VBO);
+
+    glBindVertexArray(VAO[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+
+    GLfloat rectangle[] = {
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*) 0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    SDL_Event event;
+    bool isRunning = true;
+    float counter = -1.0f; 
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    hello.action();
+
+                    counter = - counter;
+
+                    rectangle[0] = -0.5f + counter * 0.1f;
+                    rectangle[1] = -0.5f + counter * 0.1f;
+                    rectangle[3] = -0.5f + counter * 0.1f;
+                    rectangle[4] = 0.5f + counter * 0.1f;
+                    rectangle[6] = 0.5f + counter * 0.1f;
+                    rectangle[7] = 0.5f + counter * 0.1f;
+                    rectangle[9] = 0.5f + counter * 0.1f;
+                    rectangle[10] = -0.5f + counter * 0.1f;
+                    
+                    break;
+            }
+
+            glClearColor(0.0f, 0.67f, 0.67f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glBindVertexArray(VAO[0]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*) 0);
+            glEnableVertexAttribArray(0);
+            glDrawArrays(GL_POLYGON, 0, 4);
+            glBindVertexArray(0);
+
+            SDL_GL_SwapWindow(window);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
